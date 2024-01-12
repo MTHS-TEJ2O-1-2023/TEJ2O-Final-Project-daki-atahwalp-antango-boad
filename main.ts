6abf6a8ff3588061b6e1a3641ab2138a58c2a2cb
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2024-01-12 12:52:19 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,21 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let signal : number = 0
+
+// on start
+radio.setGroup(11)
+radio.setTransmitPower(1)
+basic.forever(function () {
+    radio.sendString("1")
+    basic.pause(200)
+})
+
+radio.onReceivedString(function (receivedString) {
+  signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+  led.plotBarGraph(
+    Math.map(signal, -95, -42, 0, 9),
+    9
+  )
+})
